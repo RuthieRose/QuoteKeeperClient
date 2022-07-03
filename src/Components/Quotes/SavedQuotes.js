1ea5de6c97571af019e1b73e08f329e846659525
lilyruth <doodlepath@gmail.com>
2022-07-03 16:27:16 -0500

    Added delete functionality

--- src/Components/Quotes/SavedQuotes.js
+++ src/Components/Quotes/SavedQuotes.js
@@ -1,80 +1,98 @@
-import { useEffect, useState, useCallback } from 'react'
+import { useEffect, useState } from 'react'
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom'
 import axiosAPI from 'axios'
-import { useContextAccessToken, useContextUserId, useContextName} from '../Context'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useContextAccessToken, useContextUserId, useContextName } from '../Context'
 import './saved.css'
 
 
 export default function SavedQuotes({ setDisplay }) {
 
- let [index, setIndex] = useState(0)
- let [quoteId, setQuoteId] = useState('')
- let [array, setArray] = useState([])
+  let [array, setArray] = useState([])
+  let [deleted, setDeleted] = useState(0)
 
- const token = useContextAccessToken()
- const userId = useContextUserId()
- const name = useContextName()
-
- 
- let baseURL =  'https://quotekeeper.herokuapp.com'
- const axios = axiosAPI.create({
-  baseURL: 'https://quotekeeper.herokuapp.com',
-  headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` }
- })
- 
-
- useEffect(() => {
-  handleQuotes()
- }, [])
-
- useEffect(() => {
-   console.log(array)
- }, [array])
+  const token = useContextAccessToken()
+  const userId = useContextUserId()
+  const name = useContextName()
 
 
- const handleQuotes = async () => {
-   
-  try {
-    let response = await axios.get(`quotes/${userId}`)
-     response = response.data
-     console.log(response)
-     response = response.map(set => {
-      return [set['quote'], set['author'], set['_id']]
-     })
+  let baseURL = 'https://quotekeeper.herokuapp.com'
+  const axios = axiosAPI.create({
+    baseURL: 'https://quotekeeper.herokuapp.com',
+    headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` }
+  })
+
+
+  useEffect(() => {
+    handleQuotes()
+  }, [deleted])
+
+  useEffect(() => {
+    console.log(array)
+  }, [array])
+
+
+  const handleQuotes = async () => {
+
+    try {
+      let response = await axios.get(`quotes/${userId}`)
+      response = response.data
+      console.log(response)
+      response = response.map(set => {
+        return [set['quote'], set['author'], set['_id']]
+      })
       setArray(prev => [...response])
-  }
-  
-  catch (err) {
+    }
+
+    catch (err) {
       console.log(err)
     }
- }
+  }
+
+  const deleteQuote = async (e) => {
+    console.log('going to delete this quote', e.currentTarget.id)
+    let id = e.currentTarget.id
+    try {
+      let response = await axios.delete(`quotes/${userId}/${id}`)
+      setDeleted(prev => prev + 1)
+    }
+
+    catch (err) {
+      console.log(err)
+    }
+  }
 
 
- const handleHome = () => {
+const handleHome = () => {
   setDisplay(true)
- }
+}
 
- useEffect(() => {
+useEffect(() => {
   setDisplay(false)
- }, [])
- 
- let quoteList = array.map((set, index) => {
+}, [])
+
+let quoteList = array.map((set, index) => {
 
   let quote = set[0]
   let author = set[1]
-   return (
-     <div key={index}>
-       <h3>{quote}</h3>
-       <p>{author}</p>
-     </div>
-   )
- })
+  let id = set[2]
+  return (
+    <div key={id}>
+      <div> 
+        <FontAwesomeIcon icon={faTimes} onClick={deleteQuote} id={id}/>
+        </div>
+      <h3>{quote}</h3>
+      <p>{author}</p>
+    </div>
+  )
+})
 
- return (
+return (
   <>
-   <h2>{name}'s Quotes</h2>
-   <main>{quoteList}</main>
-   <div onClick={handleHome}><Link to='/'>Back to Quotes</Link></div>
+    <h2>{name}'s Quotes</h2>
+    <main>{quoteList}</main>
+    <div onClick={handleHome}><Link to='/'>Back to Quotes</Link></div>
   </>
- )
+)
 }
